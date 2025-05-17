b9a0f6b9eb9e213cdb59515aeaaccafa3dd1259c
A.M. <abbi.murcia@gmail.com>
2025-05-17 14:57:39 +0200

    Adding the signup system (with MVC files : model, view, control)

---
+++ Includes/signup_model.php
@@ -0,0 +1,40 @@
+<?php
+
+//signup_model.php is for the interaction with the database
+
+declare(strict_types=1);
+
+function get_email(object $pdo, string $email){
+    $query = 'SELECT email FROM users WHERE email = :email;';
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);//Use the name of the column inside the database
+    return $result;
+}
+
+function get_username(object $pdo, string $username) {
+    $query = 'SELECT username FROM users WHERE username = :username;';
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);//Use the name of the column inside the database
+    return $result;
+}
+
+function set_user(object $pdo, string $lname, string $fname, string $username, string $email, string $pwd){
+    $query = 'INSERT INTO users (lname, fname, username, email, pwd) VALUES (:lname, :fname, :username, :email, :pwd);';
+    $stmt = $pdo->prepare($query);
+
+    $options = ['cost' => 12];
+    $hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT, $options);
+
+    $stmt->bindParam(':lname', $lname);
+    $stmt->bindParam(':fname', $fname);
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':pwd', $hashed_pwd);
+    $stmt->execute();
+}
---
+++ Includes/singup_inc.php
@@ -0,0 +1,66 @@
+<?php
+
+
+//Verify how the user got acess to the page
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $lname = $_POST['name'];
+    $fsname = $_POST['surname'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
+
+    try {
+
+        require_once 'DBlink.php';
+        require_once 'signup_model.php';
+        require_once 'signup_contrphp';
+
+        // Error Handlers
+            $errors = [];
+
+            // Check if inputs are empty
+            if (is_input_empty($lname, $fsname, $username, $email, $pwd)) {
+                $errors['empty_input'] = 'Remplissez tous les champs.';
+            }
+
+            // Check if email is valid
+            if (is_email_invalid($email)) {
+                $errors['invalid_email'] = 'Email invalide.';
+            }
+
+            // Check if email is registered
+            if (is_email_registered($pdo, $email)) {
+                $errors['email_used'] = 'Email déjà enregistré.';
+            }
+
+            // Check if username is taken
+            if (is_username_taken($pdo, $username)) {
+                $errors['username_taken'] = 'Nom d\'utilisateur déjà pris.';
+            }
+
+        require_once 'config_session.inc.php';
+
+        if ($error){
+            $_SESSION['errors_signup'] = $errors;
+            header("Location: ../signup.php");
+            die();
+        }
+
+        create_user($pdo, $lname, $fsname, $username, $email, $pwd);
+
+        header("Location: ../index.php?signup=sucess");
+
+        $pdo = null; // Close the connection
+        $stmt = null; // Close the statement
+
+        die();
+
+    } catch (PDOException $e) {
+        die('Query failed: ' . $e->getMessage());
+    }
+
+} else {
+    header("Location: ../index.php");
+    die();
+};
